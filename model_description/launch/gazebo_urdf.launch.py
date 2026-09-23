f842a3a13b860cf387debeb4842573c9be173893
Jefalisson <jefferson.alisson@hotmail.com>
2026-09-23 18:07:26 -0300

    feat: atualiza model_description com SDF e URDF da Prática 02

--- model_description/launch/gazebo_urdf.launch.py
+++ model_description/launch/gazebo_urdf.launch.py
@@ -4,20 +4,18 @@ from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Aponta para o arquivo SDF do seu robô vascaíno
-    model_path = os.path.expanduser('~/.gazebo/models/my_robot/model.sdf')
+    # Caminho direto para o ficheiro URDF que acabámos de criar
+    urdf_file = os.path.expanduser('~/ros2_ws/src/model_description/urdf/vasco_robot.urdf')
     
     return LaunchDescription([
-        # Inicia o servidor e o cliente do Gazebo
         ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
             output='screen'
         ),
-        # Nó do ROS2 para injetar o robô no Gazebo
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
-            arguments=['-entity', 'vasco_robot', '-file', model_path, '-x', '0', '-y', '0', '-z', '0.2'],
+            arguments=['-entity', 'vasco_robot', '-file', urdf_file, '-x', '0', '-y', '0', '-z', '0.2'],
             output='screen'
         )
     ])
